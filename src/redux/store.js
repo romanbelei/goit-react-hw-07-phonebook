58a078d52b513e86958d74188cf20da750b40e95
romanbelei <45937404+romanbelei@users.noreply.github.com>
2022-06-15 21:14:27 +0200

    ok

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, createReducer } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactApi } from './Contacts/contactSlice';
 import { reducer } from './reducer';
 
